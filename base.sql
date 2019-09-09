02b678b270b63c5aaaa879f4115edec450a063fd
DevCo <devcolombia@gmail.com>
2019-09-09 11:50:36 -0500

    Update base.sql

--- base.sql
+++ base.sql
@@ -1,9 +1,11 @@
-CREATE TABLE 'usuarios'(
-    'id' int(11) NOT NULL auto_increment,
-    'nombre' varchar (50) NOT NULL,
-    'email' varchar(50) NOT NULL,
-    'user' varchar (40) NOT NULL,
-    'password' varchar (100) NOT NULL,
-    PRIMARY KEY ('id')
+CREATE DATABASE base;
+USE base;
+CREATE TABLE usuarios(
+     id INT(11) NOT NULL auto_increment,
+     nombre VARCHAR (50) NOT NULL,
+     email VARCHAR(50) NOT NULL,
+     user VARCHAR (40) NOT NULL,
+     password VARCHAR (100) NOT NULL,
+     PRIMARY KEY (id)
 )
     ENGINE= InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
